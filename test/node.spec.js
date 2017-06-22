7408253f16a94fbaa0c1db7fb76ca9d5d3a7f914
Joel Brubaker <pk.joel@gmail.com>
2017-06-21 22:27:29 -0700

    Updated firebase-model, WIP

--- test/node.spec.js
+++ test/node.spec.js
@@ -1,17 +1,18 @@
 const assert = require('assert')
 const Model = require('firebase-model')
 const Node = require('../lib/models/node')
+const firebase = require('firebase-admin')
 const serviceAccount = require('../service-account.json')
 
-Model.init(admin => {
-  return {
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://spice-traders-core.firebaseio.com"
-  }
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://spice-traders-core.firebaseio.com"
 })
 
+Model.init(firebase)
+
 describe('Node', function() {
-  it('should have a method to create a new chart', done => {
+  it('should have a method to create a new node', done => {
     let node = new Node()
     node.create({
       'name': 'Earth',
@@ -22,12 +23,10 @@ describe('Node', function() {
         y: 0
       }
     }).then(node => {
-      console.log(node.getAttributes())
       assert(node.name === 'Earth')
       done()
     }).catch(error => {
-      console.log(error)
-      done()
+      done(error)
     })
   })
 })
